93905a3f54f6883c454c4f90a18187d66e396567
Vicky <yashkrish005@gmail.com>
2025-02-28 12:26:32 +0530

    removed commented code

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,3 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const Quiz());
 }
-
-// import 'package:adv_basics/start_screen.dart';
-// import 'package:flutter/material.dart';
-
-// void dummy() {}
-// void main() {
-//   runApp(
-//     MaterialApp(
-//       home: Scaffold(
-//         backgroundColor: const Color.fromARGB(255, 90, 14, 107),
-//         body: Center(child: StartScreen()),
-//       ),
-//     ),
-//   );
-// }
