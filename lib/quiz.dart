e8e5b4ac51d0e6561929796e61073953f41deaa8
Vicky <yashkrish005@gmail.com>
2025-03-01 10:56:02 +0530

    Questions and Answers in Results Screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,5 +1,6 @@
 import 'package:adv_basics/data/questions.dart';
 import 'package:adv_basics/questions_screen.dart';
+import 'package:adv_basics/results_screen.dart';
 import 'package:adv_basics/start_screen.dart';
 
 import 'package:flutter/material.dart';
@@ -41,20 +42,22 @@ class _QuizState extends State<Quiz> {
     selectedAnswers.add(ans);
     if (selectedAnswers.length == questions.length) {
       setState(() {
-        activeScreen = 'Start-Screen';
+        activeScreen = 'Results-Screen';
       });
     }
   }
 
   @override
   Widget build(ctx) {
-    final screenChoice =
+    Widget screenChoice =
         activeScreen == 'Start-Screen'
             ? StartScreen(switchScreen)
             : QuestionsScreen(
               onSelectAnswer: chooseAnswer,
             ); // passing function as a parameter
-
+    if (activeScreen == 'Results-Screen') {
+      screenChoice = ResultsScreen(chosenAnswers: selectedAnswers);
+    }
     return MaterialApp(
       home: Scaffold(
         body: Container(
--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,10 +1,42 @@
+import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/questions_summary.dart';
 import 'package:flutter/material.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key});
+  const ResultsScreen({super.key, required this.chosenAnswers});
+
+  final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0], // fcfs
+        'user_answer': chosenAnswers[i],
+      });
+    }
+    return summary;
+  }
 
   @override
   Widget build(ctx) {
-    return Column(children: [Text('data')]);
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('X / Y questions are Right !!'),
+            const SizedBox(height: 30),
+            QuestionsSummary(getSummaryData()),
+            const SizedBox(height: 30),
+            TextButton.icon(onPressed: () {}, label: Text('Retry ?')),
+          ],
+        ),
+      ),
+    );
   }
 }
